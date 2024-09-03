f51bf38cf1009ee3324d073c9f2392beb6dbd2ba
Issaka Faisal <faisal@mpharma.com>
2024-09-03 11:12:11 +0000

    build patient system

---
+++ lib/appwrite.config.ts
@@ -0,0 +1,20 @@
+import * as sdk from 'node-appwrite';
+
+const client = new sdk.Client();
+
+export const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT;
+export const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+
+export const DATABASE_ID = process.env.DATABASE_ID;
+export const PATIENT_COLLECTION_ID = process.env.PATIENT_COLLECTION_ID;
+export const APPOINTMENT_COLLECTION_ID = process.env.APPOINTMENT_COLLECTION_ID;
+export const BUCKET_ID = process.env.BUCKET_ID;
+
+client.setEndpoint(ENDPOINT!).setProject(PROJECT_ID!).setKey(API_KEY!);
+
+export const databases = new sdk.Databases(client);
+export const storage = new sdk.Storage(client);
+export const functions = new sdk.Functions(client);
+export const messaging = new sdk.Messaging(client);
+export const users = new sdk.Users(client);
